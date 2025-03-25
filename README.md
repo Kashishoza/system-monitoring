7eafdd3b05658fb364432c9ef12b6e2400a0f8b6
Kashish Oza <90240675+Kashishoza@users.noreply.github.com>
2025-03-25 06:50:21 +0000

    Updated Code

--- README.md
+++ README.md
@@ -15,3 +15,26 @@ This Bash script monitors system performance and logs the following system infor
 
 The collected data is stored in a log file located at logs/monitor.log.
 
+Run the script using:
+```bash
+./monitor.sh
+```
+
+If you see a "Permission denied" error, ensure you have execution rights:
+```bash
+chmod +x "monitor.sh"
+```
+
+
+## Automating with Cron Job
+
+To run the script automatically every 5 minutes:
+```bash
+crontab -e
+```
+
+Then add the following line to run the script every 5 minutes:
+
+```bash
+*/5 * * * * /path/to/your/script/monitor.sh
+```
--- monitor.sh
+++ monitor.sh
@@ -8,23 +8,26 @@ running_process=$(ps -eo pid,comm,%cpu,%mem --sort=%cpu | head -6)
 
 LOG_FILE="logs/monitor.log"
 
-if [[ ! -d $LOG_FILE ]]; then
+if [[ ! -d  ]]; then
     mkdir logs
 fi
 
 # create a log file
-echo "System Monitoring Log - $(date)" > $LOG_FILE
-echo "===========System Uptime=============" >> $LOG_FILE
-echo "$sytem_uptime" >> $LOG_FILE
+{
+echo "System Monitoring Log - $(date)" > 
+echo "===========System Uptime=============" >> 
+echo "$sytem_uptime" >> 
 
-echo "===========Cpu Usage=============" >> $LOG_FILE
-echo "$cpu_usage"  >> $LOG_FILE
+echo "===========Cpu Usage=============" >> 
+echo "$cpu_usage"  >> 
 
-echo "===========Memory Usage=============" >> $LOG_FILE
-echo "$memory_usage" >> $LOG_FILE
+echo "===========Memory Usage=============" >> 
+echo "$memory_usage" >> 
 
-echo "===========Disk Usage=============" >> $LOG_FILE
-echo "$disk_usage" >> $LOG_FILE
+echo "===========Disk Usage=============" >> 
+echo "$disk_usage" >> 
 
-echo "===========Top 5 running process=============" >> $LOG_FILE
-echo "$running_process" >> $LOG_FILE
+echo "===========Top 5 running process=============" >> 
+echo "$running_process" >>
+echo "" 
+} >> $LOG_FILE
